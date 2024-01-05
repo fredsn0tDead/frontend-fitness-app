a0d15892269ca62e00a344960900f30a295bfeb5
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2024-01-05 14:11:54 -0500

    Edited the gifs so they have now become clickable

--- src/Components/RM.jsx
+++ src/Components/RM.jsx
@@ -1,25 +1,46 @@
 import React from 'react'
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Grid } from '@mui/material';
+import { useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Grid ,Card} from '@mui/material';
 
 export const RM = ({ isOpen, handleClose ,excerise_array,excerise_description}) => {
+  const [selectedGif, setSelectedGif] = useState(null);
+
+  const handleGifClick = (index) => {
+    setSelectedGif(index);
+  };
+
+  const handleModalClose = () => {
+    setSelectedGif(null);
+    handleClose();
+  };
+  const handleLearnMoreClick = () => {
+    // Handle click on the selected GIF, e.g., navigate to a new modal with more information.
+    // Add your logic here.
+  };
   return (
-    <Dialog open={isOpen} onClose={handleClose}>
+    <Dialog open={isOpen} onClose={handleModalClose}>
       <DialogTitle>{excerise_description}</DialogTitle>
       <DialogContent>
-        
         <Grid container spacing={2}>
-          {excerise_array.map((image, index) => (//all the gifs are put in an array and then mapped to the grid
+          {excerise_array.map((image, index) => (
             <Grid item xs={6} sm={4} md={3} key={index}>
-              <img src={image} alt={`Image ${index + 1}`} style={{ maxWidth: '100%' }} />
+              <Card onClick={() => handleGifClick(index)}>
+                <img src={image} alt={`Image ${index + 1}`} style={{ maxWidth: '100%', cursor: 'pointer' }} />
+              </Card>
             </Grid>
           ))}
         </Grid>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button onClick={handleModalClose} color="primary">
           Close
         </Button>
+        {selectedGif !== null && (
+          <Button onClick={handleLearnMoreClick} color="primary">
+            Learn More
+          </Button>
+        )}
       </DialogActions>
     </Dialog>
-  )
-}
+  );
+};
